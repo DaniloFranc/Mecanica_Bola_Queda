d88f4289169f2df8662be2a1c1623030622a7532
DaniloFranc <danilofranco2011@hotmail.com>
2023-04-18 16:08:20 -0300

    Atualizar Bolinha para bola de Basquete

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Teste_bola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picBola = new System.Windows.Forms.PictureBox();
+            this.picRaquete = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_re = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBola)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRaquete)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBola
+            // 
+            this.picBola.BackColor = System.Drawing.Color.White;
+            this.picBola.Image = ((System.Drawing.Image)(resources.GetObject("picBola.Image")));
+            this.picBola.Location = new System.Drawing.Point(398, 112);
+            this.picBola.Name = "picBola";
+            this.picBola.Size = new System.Drawing.Size(32, 33);
+            this.picBola.TabIndex = 1;
+            this.picBola.TabStop = false;
+            // 
+            // picRaquete
+            // 
+            this.picRaquete.BackColor = System.Drawing.Color.Black;
+            this.picRaquete.Location = new System.Drawing.Point(251, 429);
+            this.picRaquete.Name = "picRaquete";
+            this.picRaquete.Size = new System.Drawing.Size(325, 10);
+            this.picRaquete.TabIndex = 2;
+            this.picRaquete.TabStop = false;
+            this.picRaquete.Click += new System.EventHandler(this.picRaquete_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 321);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 7;
+            // 
+            // btn_re
+            // 
+            this.btn_re.Location = new System.Drawing.Point(583, 79);
+            this.btn_re.Name = "btn_re";
+            this.btn_re.Size = new System.Drawing.Size(107, 79);
+            this.btn_re.TabIndex = 8;
+            this.btn_re.Text = "Reiniciar";
+            this.btn_re.UseVisualStyleBackColor = true;
+            this.btn_re.Click += new System.EventHandler(this.btn_re_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 438);
+            this.Controls.Add(this.btn_re);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picRaquete);
+            this.Controls.Add(this.picBola);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBola)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRaquete)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBola;
+        private System.Windows.Forms.PictureBox picRaquete;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_re;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste_bola
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private DateTime startTime;
+
+        public Form1(int valor)
+        {
+            InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += timer1_Tick;
+
+            if (valor == 0)
+            {
+                valor = 10;
+            }
+            else
+            {
+                picRaquete.Height = valor;
+            }
+
+        }
+
+        
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            startTime = DateTime.Now;
+            timer.Start();
+        }
+
+        double velocidade = 10.0;
+        double fatorReducao = 0.2; 
+        double incremento = 1.0;
+
+        private double velocidadeHorizontal = 0; // adicione esta linha no início da classe
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = DateTime.Now - startTime;
+            int seconds = (int)Math.Floor(elapsed.TotalSeconds);
+
+            label1.Text = string.Format("Tempo decorrido: {0} segundos", seconds);
+
+            double novaVelocidadeVertical = velocidade;
+
+            if (picBola.Top >= 401)
+            {
+                novaVelocidadeVertical = novaVelocidadeVertical * 0.80;
+            }
+            else
+            {
+                novaVelocidadeVertical = novaVelocidadeVertical + (seconds * fatorReducao);
+            }
+
+            int novaPosicaoVertical = (int)(picBola.Top + (novaVelocidadeVertical * incremento));
+
+            if (novaPosicaoVertical <= 0 || novaPosicaoVertical >= 401)
+            {
+                novaVelocidadeVertical = -novaVelocidadeVertical;
+            }
+
+            velocidade = novaVelocidadeVertical;
+
+            // Adicione as linhas abaixo para atualizar a velocidade horizontal e posição da bola
+           // velocidadeHorizontal = -1.5; // ajuste a velocidade horizontal aqui
+        //    int novaPosicaoHorizontal = (int)(picBola.Left + (velocidadeHorizontal * incremento));
+          //  picBola.Left = novaPosicaoHorizontal;
+
+            picBola.Top = novaPosicaoVertical;
+            label3.Text = picBola.Top.ToString();
+            label4.Text = picBola.Left.ToString();
+        }
+
+        private void btn_re_Click(object sender, EventArgs e)
+        {
+            
+            startTime = DateTime.Now;
+
+            
+            picBola.Top = 140;
+            picBola.Left = 398;
+            velocidade = 15.0;
+
+            
+            label1.Text = "Tempo decorrido: 0 segundos";
+
+        }
+
+        private void picRaquete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
